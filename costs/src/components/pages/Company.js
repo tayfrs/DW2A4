e1f1053bd4f6debfa988120956572c31d0f40d37
tayfrs <tayna.floor@gmail.com>
2021-12-09 19:44:23 -0300

    atualização de paginas

--- costs/src/components/pages/Company.js
+++ costs/src/components/pages/Company.js
@@ -1,6 +1,14 @@
 import React from 'react'
 function Company(){
-    return <h1>Company</h1>
+    return (
+        <div>
+            <p className='paragrafo'>Pipefy permite que pessoas do mundo todo criem e executem processos de trabalho eficientes</p>
+            <p>
+            COSTS é a Plataforma de Gerenciamento de Projetos que organiza e controlar seus trabalho em um único lugar. Assim, todos podem automatizar e agilizar qualquer processo por conta própria.
+            Não há limites para profissionais com as ferramentas certas para melhorar seu trabalho, sem depender de ninguém. É por isso que o Costs aumenta a agilidade e a eficiência e oferece maior valor por meio da otimização de processos de trabalho, proporcionando melhores resultados.
+            </p>
+        </div>
+    )
 }
 
 export default Company
